c53663cfc8734607560970c55e4658ff5bdf69a2
Denis Goncharov <gonchds@gmail.com>
2023-09-11 11:31:54 +0300

    init project

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,27 @@
+import { Configuration } from 'webpack';
+import { BuildOptions } from './types/config';
+import { buildLoaders } from './buildLoaders';
+import { buildPlugins } from './buildPlugins';
+import { buildResolvers } from './buildResolvers';
+import { buildDevServer} from './buildDevServer';
+
+export function buildWebpackConfig(options: BuildOptions): Configuration {
+  const { mode, paths, isDev } = options;
+
+  return {
+    mode,
+    entry: paths.entry,
+    output: {
+      path: paths.build,
+      filename: '[name].[contenthash].js',
+      clean: true,
+    },
+    module: {
+      rules: buildLoaders(options),
+    },
+    resolve: buildResolvers(),
+    plugins: buildPlugins(options),
+    devtool: isDev ? 'inline-source-map' : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
+  };
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/index.scss';
+
+const App = () => {
+  return (
+    <div className="app light">
+      <h1>Spellchecker</h1>
+    </div>
+  );
+};
+
+export default App;
